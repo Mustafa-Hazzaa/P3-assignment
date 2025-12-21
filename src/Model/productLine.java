09aa505224576ff32c0146ca66fbb3f4c080720c
Mustafa-Hazzaa <mustafahazzaa06@gmail.com>
2025-12-21 17:02:45 +0300

    forgot things

--- src/Model/productLine.java
+++ src/Model/productLine.java
@@ -8,5 +8,35 @@ public class productLine {
     String status;
     List<Task> tasks;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
